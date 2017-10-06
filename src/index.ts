4bfec9c33d411fe5b423c2cb170581e46dda2581
yenshih <yenshih95@gmail.com>
2017-10-06 15:13:55 +0800

    Fix some comments

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Middleware } from 'redux';
 import awaiters from './awaiters';
 
 export const createAwaiterMiddleware = (): Middleware => () => next => (action) => {
-    awaiters.consume(action as any); // cast AnyAction to standard Action<T>
+    awaiters.consume(action as any); // cast AnyAction to standard Action<P, M>
     return next(action);
 };
 
